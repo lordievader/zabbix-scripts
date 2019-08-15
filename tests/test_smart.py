be8ac31a7d9545028a06e8a0e297aa568313e12f
Olivier van der Toorn <oliviervdtoorn@gmail.com>
2019-08-15 20:19:34 +0200

    Restructure folder
    Introduce module folder and test folder.
    Add additional discovery tests, a few discovery scripts were left untested.

---
+++ tests/test_smart.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+"""Author:      Olivier van der Toorn <oliviervdtoorn@gmail.com>
+Description:    Collection of tests for the Zabbix smart script.
+"""
+# pylint: disable=import-error,unused-import
+from pytest_mock import mocker  # noqa: F401
+
+from zabbix_scripts import smart
+
+
+# pylint: disable=line-too-long
+MOCK_OUTPUT = """SMART Attributes Data Structure revision number: 16
+Vendor Specific SMART Attributes with Thresholds:
+ID# ATTRIBUTE_NAME          FLAG     VALUE WORST THRESH TYPE      UPDATED  WHEN_FAILED RAW_VALUE
+  1 Raw_Read_Error_Rate     0x002f   200   200   051    Pre-fail  Always       -       0
+  3 Spin_Up_Time            0x0027   171   171   021    Pre-fail  Always       -       6441
+  4 Start_Stop_Count        0x0032   100   100   000    Old_age   Always       -       11
+  5 Reallocated_Sector_Ct   0x0033   200   200   140    Pre-fail  Always       -       0
+  7 Seek_Error_Rate         0x002e   200   200   000    Old_age   Always       -       0
+  9 Power_On_Hours          0x0032   087   087   000    Old_age   Always       -       9661
+ 10 Spin_Retry_Count        0x0032   100   253   000    Old_age   Always       -       0
+ 11 Calibration_Retry_Count 0x0032   100   253   000    Old_age   Always       -       0
+ 12 Power_Cycle_Count       0x0032   100   100   000    Old_age   Always       -       10
+192 Power-Off_Retract_Count 0x0032   200   200   000    Old_age   Always       -       4
+193 Load_Cycle_Count        0x0032   155   155   000    Old_age   Always       -       137291
+194 Temperature_Celsius     0x0022   113   099   000    Old_age   Always       -       37
+196 Reallocated_Event_Count 0x0032   200   200   000    Old_age   Always       -       0
+197 Current_Pending_Sector  0x0032   200   200   000    Old_age   Always       -       0
+198 Offline_Uncorrectable   0x0030   200   200   000    Old_age   Offline      -       0
+199 UDMA_CRC_Error_Count    0x0032   200   200   000    Old_age   Always       -       0
+200 Multi_Zone_Error_Rate   0x0008   200   200   000    Old_age   Offline      -       0"""  # noqa: E501
+
+
+def test_smart_no_arguments():
+    """Tests if no arguments gives a zero.
+    """
+    assert smart.main() == 0
+
+
+# pylint: disable=redefined-outer-name
+def test_smart_single_argument(mocker):  # noqa: F811
+    """Tests if a single argument gives string output (smart output).
+    """
+    mocker.patch.object(smart, 'smartvalues')
+    smart.smartvalues.return_value = MOCK_OUTPUT
+    assert isinstance(smart.main('/dev/sda'), str)
+
+
+# pylint: disable=redefined-outer-name
+def test_smart_two_arguments(mocker):  # noqa: F811
+    """Tests the request of a value.
+    """
+    mocker.patch.object(smart, 'smartvalues')
+    smart.smartvalues.return_value = MOCK_OUTPUT
+    assert smart.main('/dev/sda', 'Power_On_Hours') != 0
---
+++ zabbix_scripts/smart.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+"""Description: Allow Zabbix to monitor smartvalues.
+Author: Olivier van der Toorn <oliviervdtoorn@gmail.com>
+"""
+import sys
+import subprocess
+import re
+
+
+def smartvalues(disk):
+    """Retrieve the smartvalues for the given disk.
+
+    :param disk: disk to check output for
+    :type disk: str
+    :return: smartctl output
+    """
+    command = ['sudo', 'smartctl', '-A', disk]
+    output = subprocess.getoutput(" ".join(command))
+    return output
+
+
+def filter_output(attribute, output):
+    """Filter the output, looking for the given attribute.
+
+    :param attribute: string to look for
+    :type attribute: str
+    :param output: output from smartctl
+    :type output: str
+    :return: line containing attribute
+    """
+    for line in output.split('\n'):
+        if attribute in line:
+            break
+
+    else:
+        line = "0"
+
+    return line
+
+
+def main(*args):
+    """Main function calls all the other functions.
+    """
+    if len(args) == 1:
+        disk = args[0]
+        return smartvalues(disk)
+
+    elif len(args) == 2:
+        disk = args[0]
+        attribute = args[1]
+        output = filter_output(
+            attribute, smartvalues(disk))
+        splitted_data = re.split(r'\s+', output.lstrip())
+        if len(splitted_data) >= 10:
+            value = splitted_data[9]
+
+        else:
+            value = 0
+
+        return value
+
+    return 0
+
+
+if __name__ == '__main__':
+    print(main(*sys.argv[1:]))
